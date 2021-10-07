e4ebf9948155002b0c4534c77db92346f5254ebc
howdy-mj <hi.minjungkim@gmail.com>
2021-10-07 22:52:50 +0900

    feat: Alert 컴포넌트 생성

---
+++ src/store/alert/reducer.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface AlertState {
+  visible: boolean;
+}
+
+const initialState: AlertState = {
+  visible: false,
+};
+
+const alertReducer = createSlice({
+  name: 'alert',
+  initialState,
+  reducers: {
+    showAlert(
+      state,
+      action: PayloadAction<{
+        visible: boolean;
+      }>,
+    ) {
+      const { visible } = action.payload;
+      return { ...state, visible };
+    },
+  },
+});
+
+export const { showAlert } = alertReducer.actions;
+
+export default alertReducer.reducer;

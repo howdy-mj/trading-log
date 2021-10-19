a531c9ba4b0f4eb6d13f00da7fc13f4d32f97d66
howdy-mj <hi.minjungkim@gmail.com>
2021-10-19 22:30:16 +0900

    feat: Pagination 컴포넌트

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import styled from '@emotion/styled';
 
-const PaginationComponent = () => {
-  return <div></div>;
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+
+interface PaginationProps {
+  totalPageNum: number;
+  currentPage: number;
+  changePage: (isPrev: boolean) => void;
+}
+
+const Pagination = ({
+  totalPageNum,
+  currentPage,
+  changePage,
+}: PaginationProps) => {
+  return (
+    <PaginationWrap>
+      <PaginationContainer>
+        <ArrowButton onClick={() => changePage(true)}>
+          <AiOutlineLeft />
+        </ArrowButton>
+        <div className="content">
+          {currentPage} / <span className="totalPage">{totalPageNum}</span>
+        </div>
+        <ArrowButton onClick={() => changePage(false)}>
+          <AiOutlineRight />
+        </ArrowButton>
+      </PaginationContainer>
+    </PaginationWrap>
+  );
 };
 
-export default PaginationComponent;
+export default Pagination;
+
+const PaginationWrap = styled.div`
+  margin: 0 auto;
+  margin-top: 2rem;
+`;
+
+const PaginationContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  .content {
+    margin: 0 1rem;
+  }
+
+  .totalPage {
+    color: #8e8e8e;
+  }
+`;
+
+const ArrowButton = styled.button`
+  padding: 0.3rem;
+  border: 1px solid #888888;
+  border-radius: 6px;
+  cursor: pointer;
+  > svg {
+    display: block;
+    margin: 0;
+    width: 2rem;
+    height: 2rem;
+  }
+
+  &:hover {
+    background-color: #e5e5e5;
+  }
+`;
--- src/pages/Main/List/index.tsx
+++ src/pages/Main/List/index.tsx
@@ -1,12 +1,16 @@
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 
-import { PREDICT } from '~models/post.model';
+import { PREDICT, PostWithId } from '~models/post.model';
 import { selectPostList } from '~store/post/selector';
 
 import Loading from '~components/Loading';
+import Pagination from '~components/Pagination';
+
+const POST_PER_PAGE = 10;
 
 interface ListProps {
   isLoading: boolean;
@@ -15,6 +19,30 @@ interface ListProps {
 const ListComponent = ({ isLoading }: ListProps) => {
   const history = useHistory();
   const postList = useSelector(selectPostList);
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPostList, setCurrentPostList] = useState<PostWithId[]>([]);
+  const [totalPageNum, setTotalPageNum] = useState<number>(0);
+
+  useEffect(() => {
+    const totalCount = postList.length;
+    if (totalCount !== 0) {
+      const indexOfLastPost = currentPage * POST_PER_PAGE;
+      const indexOfFirstPost = indexOfLastPost - POST_PER_PAGE;
+      setTotalPageNum(Math.ceil(totalCount / POST_PER_PAGE));
+      setCurrentPostList(postList.slice(indexOfFirstPost, indexOfLastPost));
+    }
+  }, [postList, currentPage]);
+
+  const changeCurrentPage = (isPrev: boolean) => {
+    if (isPrev) {
+      if (currentPage === 1) return;
+      setCurrentPage(currentPage - 1);
+    } else {
+      if (currentPage === totalPageNum) return;
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
   const linkToDetail = (id: string) => {
     history.push(`/detail/${id}`);
@@ -25,50 +53,59 @@ const ListComponent = ({ isLoading }: ListProps) => {
       {isLoading ? (
         <Loading />
       ) : (
-        <table>
-          <colgroup>
-            <col />
-            <col style={{ width: '30%' }} />
-            <col />
-            <col />
-            <col />
-          </colgroup>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>제목</th>
-              <th>예상</th>
-              <th>타겟</th>
-              <th>날짜</th>
-            </tr>
-          </thead>
-          <tbody>
-            {postList.length === 0 && (
-              <TR empty>
-                <td colSpan={5}>일지를 작성해주세요.</td>
-              </TR>
-            )}
-            {postList?.map((post, idx) => (
-              <TR key={post.id} onClick={() => linkToDetail(post.id)}>
-                <td>{idx + 1}</td>
-                <td>{post.title}</td>
-                <td>
-                  <Predict isUp={post.predict === PREDICT.UP}>
-                    {post.predict}
-                  </Predict>
-                </td>
-                <td>{post.target}</td>
-                <td>{post.createdAt.slice(0, 10)}</td>
-              </TR>
-            ))}
-          </tbody>
-          {/* <TFoot>
+        <>
+          <table>
+            <colgroup>
+              <col />
+              <col style={{ width: '30%' }} />
+              <col />
+              <col />
+              <col />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>제목</th>
+                <th>예상</th>
+                <th>타겟</th>
+                <th>날짜</th>
+              </tr>
+            </thead>
+            <tbody>
+              {postList.length === 0 && (
+                <TR empty>
+                  <td colSpan={5}>일지를 작성해주세요.</td>
+                </TR>
+              )}
+              {currentPostList?.map((post, idx) => (
+                <TR key={post.id} onClick={() => linkToDetail(post.id)}>
+                  <td>{idx + 1}</td>
+                  <td>{post.title}</td>
+                  <td>
+                    <Predict isUp={post.predict === PREDICT.UP}>
+                      {post.predict}
+                    </Predict>
+                  </td>
+                  <td>{post.target}</td>
+                  <td>{post.createdAt.slice(0, 10)}</td>
+                </TR>
+              ))}
+            </tbody>
+            {/* <TFoot>
             <tr>
               <td colSpan={4}>Total</td>
               <td>{postList.length}</td>
             </tr>
           </TFoot> */}
-        </table>
+          </table>
+          {postList.length > 10 && (
+            <Pagination
+              totalPageNum={totalPageNum}
+              currentPage={currentPage}
+              changePage={changeCurrentPage}
+            />
+          )}
+        </>
       )}
     </>
   );
